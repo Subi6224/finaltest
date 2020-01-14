d07abaa24e996e6d60ab0b09de2356afa8ac8c63
Subi6224 <868472@pdsb.net>
2020-01-14 09:56:57 -0500

    1

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function nameGenerator() {
+	let username=prompt("What your favorite snack? I'll create your perfect teddy name.");
+	if username.length=%2=0{
+		prompt("Your teddy name is Valentine.");
+	}
+	else if username.length=%3=0{
+		prompt("Your teddy name is Alex.");
+	}
+	else{
+		prompt("Your teddy name is Larry.");
+	}
+}
